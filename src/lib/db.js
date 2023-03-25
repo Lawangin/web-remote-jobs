468f01590b05ee6b2c360766ddff595e7094391b
Lawangin <lawangin.khan@gmail.com>
2023-03-25 00:07:50 -0400

    fix: fix linting on files

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,11 @@
-import { Pool } from 'pg'
+import { Pool } from 'pg';
 
 const pool = new Pool({
   host: process.env.NEXT_PUBLIC_PG_HOST,
   port: parseInt(process.env.NEXT_PUBLIC_PG_PORT),
   user: process.env.NEXT_PUBLIC_PG_USER,
   password: process.env.NEXT_PUBLIC_PG_PASSWORD,
-  database: process.env.NEXT_PUBLIC_PG_DATABASE
-})
+  database: process.env.NEXT_PUBLIC_PG_DATABASE,
+});
 
-export default pool
+export default pool;
