f29322ba3bde944bd3804d35e98d2716bdcf1b15
Lawangin <lawangin.khan@gmail.com>
2023-04-12 03:50:05 -0400

    feat: add pagination and infinite scroller

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,17 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import pool from '../../../lib/db';
 
 // eslint-disable-next-line no-unused-vars
-export async function GET(req: Request) {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
   try {
+    const url = new URL(req.url as string, `http://${req.headers.host}`);
+    const page = Number(url.searchParams.get('page')) || 1;
+    const pageSize = Number(url.searchParams.get('pageSize')) || 10;
+
+    const offset = (page - 1) * pageSize;
+
     const { rows } = await pool.query(
-      'SELECT * FROM temp_jobs ORDER BY "Date" DESC'
+      `SELECT * FROM temp_jobs ORDER BY "Date" DESC LIMIT ${pageSize} OFFSET ${offset}`
     );
-    return new Response(JSON.stringify(rows), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+
+    const totalRowsResult = await pool.query('SELECT COUNT(*) FROM temp_jobs');
+    const totalRows = parseInt(totalRowsResult.rows[0].count, 10);
+    const totalPages = Math.ceil(totalRows / pageSize);
+
+    return new Response(
+      JSON.stringify({
+        data: rows,
+        pagination: {
+          page: page,
+          pageSize: pageSize,
+          totalPages: totalPages,
+          totalItems: totalRows,
+        },
+      }),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   } catch (error) {
     if (error instanceof Error) {
       return new Response(JSON.stringify({ error: error.message }), {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,115 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Text, Box } from '@chakra-ui/react';
 import Category from './components/category';
 import TopBar from './components/TopBar';
 import DisplayData from './components/DisplayData';
 
+interface IData {
+  id: string;
+  Date: string;
+  Company: string;
+  Title: string;
+  Location: string;
+  Salary: number;
+  Description: string;
+  Level: string;
+  Type: string;
+  Function: string;
+  Industry: string;
+  Link: string;
+  image_url: string;
+}
+
 export default function Home() {
   const [loading, setLoading] = useState(true);
-  const [dashboard, setDashboard] = useState([]);
-  const [filterData, setFilterData] = useState([]);
-  const [bgColor, setBgColor] = useState(false);
+  const [dashboard, setDashboard] = useState<IData[]>([]);
+  const [filterData, setFilterData] = useState<IData[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [observer, setObserver] = useState<IntersectionObserver | null>(null);
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+  const [bgColor, setBgColor] = useState<boolean>(false);
+  const [loadingMore, setLoadingMore] = useState<boolean>(false);
 
   useEffect(() => {
-    async function fetchDashboard() {
-      const response = await fetch('/api/data');
-      const data = await response.json();
-      setDashboard(data);
-      setFilterData(data);
+    (async () => {
+      setLoading(true);
+      await fetchDashboard(currentPage);
       setLoading(false);
+    })();
+  }, []);
+
+  async function fetchDashboard(page: number) {
+    const response = await fetch(`/api/data?page=${page}&pageSize=10`);
+    const data = await response.json();
+    setDashboard(prevState => {
+      const updatedState = [...prevState, ...data.data];
+      setFilterData(updatedState);
+      setLoadingMore(false);
+      return updatedState;
+    });
+    console.log(currentPage);
+  }
+
+  /**
+   * Intersection observer that checks to see when we hit the bottom of the page to load more results
+   */
+  useEffect(() => {
+    // Disconnect the previous observer if it exists
+    if (observer) {
+      observer.disconnect();
     }
 
-    fetchDashboard();
-  }, []);
+    // Set the options for the IntersectionObserver
+    const options = {
+      root: null, // Observe the entire viewport
+      rootMargin: '0px', // No margin around the root
+      threshold: 0.7, // Trigger the callback when 100% of the target is visible
+    };
+
+    // Create a new IntersectionObserver with the provided options
+    const newObserver = new IntersectionObserver(entries => {
+      // Loop through each entry (in this case, only one entry)
+      entries.forEach(entry => {
+        // Check if the entry is intersecting the viewport
+        if (entry.isIntersecting && !loadingMore) {
+          // If it is, update the current page and fetch the data for the next page
+          setLoadingMore(true);
+          // fetchDashboard(currentPage + 1);
+          setCurrentPage(prevPage => {
+            fetchDashboard(prevPage + 1);
+            return prevPage + 1;
+          });
+          setLoadingMore(false);
+          // fetchDashboard(dashboard.length / 10 + 1);
+        }
+      });
+    }, options);
+
+    // If loadMoreRef.current is not null (meaning the ref is attached to an element),
+    // start observing that element
+    if (loadMoreRef.current) {
+      newObserver.observe(loadMoreRef.current);
+    }
+
+    // Set the observer state to the new observer
+    setObserver(newObserver);
+
+    // Cleanup function: disconnect the new observer when the component is unmounted or
+    // when the dependencies change
+    return () => {
+      newObserver.disconnect();
+    };
+  }, []); // Run the effect when loadMoreRef.current changes
 
   function handleBgColor() {
     setBgColor(!bgColor);
   }
 
-  interface mydata {
-    id: string;
-    Date: string;
-    Company: string;
-    Title: string;
-    Location: string;
-    Salary: number;
-    Description: string;
-    Level: string;
-    Type: string;
-    Function: string;
-    Industry: string;
-    Link: string;
-    image_url: string;
-  }
-
-  function handleFilterData(filterTerm: string, e: any): void {
+  function handleFilterData(filterTerm: string, e: MouseEvent): void {
     e.preventDefault();
-    const newData = dashboard.filter((data: mydata) =>
+    const newData = dashboard.filter((data: IData) =>
       data.Title.toLowerCase().includes(filterTerm.toLowerCase())
     );
     setFilterData(newData);
@@ -54,7 +117,7 @@ export default function Home() {
 
   const dashboardList =
     filterData &&
-    filterData.map((data: mydata, ind: number) => (
+    filterData.map((data: IData, ind: number) => (
       <DisplayData
         key={ind}
         Title={data.Title}
@@ -75,9 +138,9 @@ export default function Home() {
   return (
     <Box>
       <TopBar />
-      {filterData && (
+      {dashboard && (
         <Category
-          count={filterData.length}
+          count={dashboard.length}
           handleFilterData={handleFilterData}
         />
       )}
@@ -88,6 +151,12 @@ export default function Home() {
       ) : (
         dashboardList
       )}
+      {loadingMore && (
+        <Text textAlign="center" mt="20px" fontSize="20px">
+          Loading more...
+        </Text>
+      )}
+      <div ref={loadMoreRef} />
     </Box>
   );
 }
