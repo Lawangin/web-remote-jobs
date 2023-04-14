0a7a556f2311b35561cf6e46c1c275fb1c99c5fd
Lawangin <lawangin.khan@gmail.com>
2023-04-14 00:45:59 -0400

    feat: add api route to filter data by keyword

---
+++ src/app/api/data/filter/route.ts
@@ -0,0 +1,61 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import pool from '../../../../lib/db';
+
+// eslint-disable-next-line no-unused-vars
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const url = new URL(req.url as string, `http://${req.headers.host}`);
+    const search = url.searchParams.get('search');
+    const page = Number(url.searchParams.get('page')) || 1;
+    const pageSize = Number(url.searchParams.get('pageSize')) || 10;
+
+    const offset = (page - 1) * pageSize;
+
+    const { rows } = await pool.query(
+      `SELECT * FROM temp_jobs WHERE "Title" ILIKE '%${search}%' ORDER BY "Date" DESC LIMIT ${pageSize} OFFSET ${offset}`
+    );
+
+    const totalRowsResult = await pool.query(
+      `SELECT COUNT(*) FROM temp_jobs WHERE "Title" ILIKE '%${search}%'`
+    );
+    const totalRows = parseInt(totalRowsResult.rows[0].count, 10);
+    const totalPages = Math.ceil(totalRows / pageSize);
+
+    return new Response(
+      JSON.stringify({
+        data: rows,
+        pagination: {
+          page: page,
+          pageSize: pageSize,
+          totalPages: totalPages,
+          totalItems: totalRows,
+        },
+      }),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(JSON.stringify({ error: error.message }), {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    } else {
+      return new Response(
+        JSON.stringify({ error: 'An unexpected error occured.' }),
+        {
+          status: 500,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+    }
+  }
+}
